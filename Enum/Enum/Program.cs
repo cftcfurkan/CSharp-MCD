abf732220d178b61bfea23ca2e824a6a854d1903
cftcfurkan <90473630+cftcfurkan@users.noreply.github.com>
2022-02-10 17:57:44 +0300

    Add files via upload

---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Days.Wednesday);
+            Console.WriteLine((int)Days.Thursday);// that's showing the day's index (hint: wednesday is third day in a week but it's index is 2)
+
+
+            int temp = 40;
+            if (temp <= (int)Temperature.Hot && temp >= (int)Temperature.Cold)
+            {
+                Console.WriteLine("what a gorgeous day!! Let's go out");
+
+            }
+
+        }
+        enum Days
+        {
+           Monday,
+           Tuesday,
+           Wednesday,
+           Thursday,
+           Friday,
+           Saturday,
+           Sunday
+        }
+        enum Temperature
+        {
+            VeryCold = -5,
+            Cold = 5,
+            Normal = 15,
+            Hot=30,
+
+            
+        }
+    }
+}
